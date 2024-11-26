5c15dee7a4629dcef5f3693ca481e24c348234a3
Pereira-234 <130488369+Pereira-234@users.noreply.github.com>
2024-11-26 11:13:38 -0300

    Update cliente.py

--- projeto encapsular/models/cliente.py
+++ projeto encapsular/models/cliente.py
@@ -61,3 +61,72 @@ class Cliente:
 
     def _str_(self):
         return f"{self._nome} - {self.email} - {self._fone}"
+
+# Persistência
+class Clientes:
+  objetos = []    # atributo estático
+
+  @classmethod
+  def inserir(cls, obj):
+    cls.abrir()
+    m = 0
+    for c in cls.objetos:
+      if c.id > m: m = c.id
+    obj.id = m + 1
+    cls.objetos.append(obj)
+    cls.salvar()
+
+  @classmethod
+  def listar_id(cls, id):
+    cls.abrir()
+    for c in cls.objetos:
+      if c.id == id: return c
+    return None  
+  
+  @classmethod
+  def atualizar(cls, obj):
+    c = cls.listar_id(obj.id)
+    if c != None:
+      c.nome = obj.nome
+      c.email = obj.email
+      c.fone = obj.fone
+      c.senha = obj.senha
+      cls.salvar()
+
+  @classmethod
+  def excluir(cls, obj):
+    c = cls.listar_id(obj.id)
+    if c != None:
+      cls.objetos.remove(c)
+      cls.salvar()
+  
+  @classmethod
+  def listar(cls):
+    cls.abrir()
+    cls.objetos.sort(key=lambda cliente: cliente.nome)
+    return cls.objetos
+
+  @classmethod
+  def salvar(cls):
+    with open("clientes.json", mode="w") as arquivo:
+        json.dump(
+            [ {"id": c.get_id(), "nome": c.get_nome(), "email": c.get_email(), "fone": c.get_fone(), "senha": c.get_senha()} for c in cls.objetos ],
+            arquivo,
+        )
+
+  @classmethod
+  def abrir(cls):
+    cls.objetos = []
+    try:
+      with open("clientes.json", mode="r") as arquivo:   # r - read
+        texto = json.load(arquivo)
+        for obj in texto:
+            c = Cliente()
+            c.set_id(obj["id"])
+            c.set_nome(obj["nome"])
+            c.set_email(obj["email"])
+            c.set_fone(obj["fone"])
+            c.set_senha(obj["senha"])
+            cls.objetos.append(c)
+    except FileNotFoundError:
+      pass
