6f4ba8a5eb8cff6844fdc3d8b2f1c9aa0396f007
Pereira-234 <130488369+Pereira-234@users.noreply.github.com>
2024-11-26 11:14:13 -0300

    Update horario.py

--- projeto encapsular/models/horario.py
+++ projeto encapsular/models/horario.py
@@ -63,3 +63,65 @@ class Horario:
 
     def _str_(self):
         return f"{self._id} - {self._data}"
+
+class Horarios:
+  objetos = []    # atributo estático
+
+  @classmethod
+  def inserir(cls, obj):
+      cls.abrir()
+      m = 0
+      for c in cls.objetos:
+          if c.id > m: m = c.id
+      obj.id = m + 1
+      cls.objetos.append(obj)
+      cls.salvar()
+
+  @classmethod
+  def listar_id(cls, id):
+    cls.abrir()
+    for c in cls.objetos:
+      if c.id == id: return c
+    return None  
+  
+  @classmethod
+  def atualizar(cls, obj):
+    c = cls.listar_id(obj.id)
+    if c != None:
+      c.data = obj.data
+      c.confirmado = obj.confirmado
+      c.id_cliente = obj.id_cliente
+      c.id_servico = obj.id_servico
+      cls.salvar()
+
+  @classmethod
+  def excluir(cls, obj):
+    c = cls.listar_id(obj.id)
+    if c != None:
+      cls.objetos.remove(c)
+      cls.salvar()
+  
+  @classmethod
+  def listar(cls):
+    cls.abrir()
+    return cls.objetos
+  
+  @classmethod
+  def salvar(cls):
+    with open("horarios.json", mode="w") as arquivo:   # w - write
+      json.dump(cls.objetos, arquivo, default = Horario.to_json)
+
+  @classmethod
+  def abrir(cls):
+      cls.objetos = []
+      try:
+          with open("horarios.json", mode="r") as arquivo:   # r - read
+              texto = json.load(arquivo)
+              for obj in texto:
+                  c = Horario(obj["id"], datetime.strptime(obj["data"], "%d/%m/%Y %H:%M"), obj.get("id_profissional", 0))
+                  c.confirmado = obj["confirmado"]
+                  c.id_cliente = obj["id_cliente"]
+                  c.id_servico = obj["id_servico"]
+                  cls.objetos.append(c)
+      except FileNotFoundError:
+          pass
