276f566bca3e9ae8a3e3f5cdfdc8948b92bbf4f1
Pereira-234 <130488369+Pereira-234@users.noreply.github.com>
2024-11-26 10:37:07 -0300

    projeto para encapsular - lista
    fazer o encapsulamento e alterações necessárias (vale nota)

---
+++ projeto encapsular/templates/abrircontaUI.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+from views import View
+import time
+
+class AbrirContaUI:
+    def main():
+        st.header("Abrir Conta no Sistema")
+        AbrirContaUI.inserir()
+
+    def inserir():
+        nome = st.text_input("Informe o nome")
+        email = st.text_input("Informe o e-mail")
+        fone = st.text_input("Informe o fone")
+        senha = st.text_input("Informe a senha", type="password")
+        confirm_senha = st.text_input("Confirme a senha", type="password")
+        if st.button("Inserir"):
+            if senha == confirm_senha:
+                View.cliente_inserir(nome, email, fone, senha)
+                st.success("Conta criada com sucesso")
+                time.sleep(2)
+                st.rerun()
+            else:
+                st.error("As senhas não coincidem")
