3c6627c6d09c56212dbaddb3bf6e05e1c5666032
Pereira-234 <130488369+Pereira-234@users.noreply.github.com>
2024-11-26 11:06:24 -0300

    Update cliente.py

--- projeto encapsular/models/cliente.py
+++ projeto encapsular/models/cliente.py
@@ -8,72 +8,56 @@ import json
 
 # Modelo
 class Cliente:
-  def __init__(self, id, nome, email, fone, senha):
-    self.id = id
-    self.nome = nome
-    self.email = email
-    self.fone = fone
-    self.senha = senha
-  def __str__(self):
-    return f"{self.nome} - {self.email} - {self.fone}"
+    def _init_(self):
+        self.__id = 0
+        self.__nome = ""
+        self.__email = ""
+        self.__fone = ""
+        self.__senha = ""
 
-# Persistência
-class Clientes:
-  objetos = []    # atributo estático
+    # Métodos SET com validações
+    def set_id(self, id):
+        self.__id = id
 
-  @classmethod
-  def inserir(cls, obj):
-    cls.abrir()
-    m = 0
-    for c in cls.objetos:
-      if c.id > m: m = c.id
-    obj.id = m + 1
-    cls.objetos.append(obj)
-    cls.salvar()
+    def set_nome(self, nome):
+        if nome.strip():
+            self.__nome = nome
+        else:
+            raise ValueError("O nome não pode ser vazio")
 
-  @classmethod
-  def listar_id(cls, id):
-    cls.abrir()
-    for c in cls.objetos:
-      if c.id == id: return c
-    return None  
-  
-  @classmethod
-  def atualizar(cls, obj):
-    c = cls.listar_id(obj.id)
-    if c != None:
-      c.nome = obj.nome
-      c.email = obj.email
-      c.fone = obj.fone
-      c.senha = obj.senha
-      cls.salvar()
+    def set_email(self, email):
+        if email.strip():
+            self.__email = email
+        else:
+            raise ValueError("O email não pode ser vazio")
 
-  @classmethod
-  def excluir(cls, obj):
-    c = cls.listar_id(obj.id)
-    if c != None:
-      cls.objetos.remove(c)
-      cls.salvar()
-  
-  @classmethod
-  def listar(cls):
-    cls.abrir()
-    cls.objetos.sort(key=lambda cliente: cliente.nome)
-    return cls.objetos
+    def set_fone(self, fone):
+        if fone.strip():
+            self.__fone = fone
+        else:
+            raise ValueError("O telefone não pode ser vazio")
 
-  @classmethod
-  def salvar(cls):
-    with open("clientes.json", mode="w") as arquivo:   # w - write
-      json.dump(cls.objetos, arquivo, default = vars)
+    def set_senha(self, senha):
+        if senha.strip():
+            self.__senha = senha
+        else:
+            raise ValueError("A senha não pode ser vazia")
 
-  @classmethod
-  def abrir(cls):
-    cls.objetos = []
-    try:
-      with open("clientes.json", mode="r") as arquivo:   # r - read
-        texto = json.load(arquivo)
-        for obj in texto:   
-          c = Cliente(obj["id"], obj["nome"], obj["email"], obj["fone"], obj["senha"])
-          cls.objetos.append(c)
-    except FileNotFoundError:
-      pass
+    # Métodos GET
+    def get_id(self):
+        return self.__id
+
+    def get_nome(self):
+        return self.__nome
+
+    def get_email(self):
+        return self.__email
+
+    def get_fone(self):
+        return self.__fone
+
+    def get_senha(self):
+        return self.__senha
+
+    def _str_(self):
+        return f"{self._nome} - {self.email} - {self._fone}"
